7c68788bf2c048b4ec7166d99497f2319441525f
wangxl <wangxl@zhenglaikeji.com>
2018-05-09 12:49:22 +0800

    Add Str_format.py

---
+++ HelloWorldV3/Str_format.py
@@ -0,0 +1,13 @@
+#coding=utf-8
+
+age=30
+name='Swoop'
+print('{0} is {1} years'.format(name,age))
+print('why is {0} playing with that python?'.format(name))
+print('{0:.3f}'.format(1.0/3))
+print('{0:_^11}'.format('hello'))
+print('{name} wrote {book}'.format(name='swoop',book='a python'))
+print('a' , end='')
+print('b' , end=' ')
+print('c')
+print('d',end='')
