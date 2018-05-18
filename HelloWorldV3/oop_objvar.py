8a5227010763c7c8be832b6922f52825bd82c496
wangxl <wangxl@zhenglaikeji.com>
2018-05-18 16:15:14 +0800

    modify oop_objvar.py

--- HelloWorldV3/oop_objvar.py
+++ HelloWorldV3/oop_objvar.py
@@ -1,37 +1,37 @@
 # coding=UTF-8
 
 class Robot:
-	"""表示有一个带名字的机器人。"""
-	population = 0
-	
-	def __init__(self,name):
-		"""初始化数据"""
-		self.name = name
-		print("(Initializing {})".format(self.name))
+    """表示有一个带名字的机器人。"""
+    population = 0
+    
+    def __init__(self,name):
+        """初始化数据"""
+        self.name = name
+        print("(Initializing {})".format(self.name))
 
-		Robot.population += 1
+        Robot.population += 1
 
-	def die(self):
-		"""我挂了"""
-		print("{} is being destroyed!".format(self.name))
+    def die(self):
+        """我挂了"""
+        print("{} is being destroyed!".format(self.name))
 
-		Robot.population -= 1
+        Robot.population -= 1
 
-		if Robot.population == 0:
-			print("{} was the last one.".format(self.name))
-		else:
-			print("There are still {:d} robots working.".format(Robot.population))
-	
-	def say_hi(self):
-		"""来自机器人的诚挚问候
-		
-		没问题，你做得到。"""
-		print("Greetings, my masters call me {}.".format(self.name))
+        if Robot.population == 0:
+            print("{} was the last one.".format(self.name))
+        else:
+            print("There are still {:d} robots working.".format(Robot.population))
+    
+    def say_hi(self):
+        """来自机器人的诚挚问候
+        
+        没问题，你做得到。"""
+        print("Greetings, my masters call me {}.".format(self.name))
 
-	@classmethod
-	def how_many(cls):
-		"""打印出当前的人口数量"""
-		print("We have {:d} robots.".format(cls.population))
+    @classmethod
+    def how_many(cls):
+        """打印出当前的人口数量"""
+        print("We have {:d} robots.".format(cls.population))
 
 droid1 = Robot("R2-D2")
 droid1.say_hi()
