8c8561979e8b30919c317d6274bc6f217b9a1b22
wangxl <wangxl@zhenglaikeji.com>
2018-05-20 18:57:10 +0800

    modify function_global.py

--- HelloWorldV3/function_global.py
+++ HelloWorldV3/function_global.py
@@ -10,3 +10,14 @@ def func():
 
 func()
 print('value of x is ',x)
+
+
+def func2(y):
+	global x
+	print('x is ',x)
+	x=y
+	print('x is ?',x)
+
+
+func2(12)
+print('value of x is still 2? ',x,sep=':')
